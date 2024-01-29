20deef90853fd9fc48b1326011f109249aa585a3
Joel Christopher  Bhatti <123950725+joelbhatti@users.noreply.github.com>
2024-01-29 19:25:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello World, We are Appedology Family')
+    res.end('Hello World, We are Appedology Family Serving the TECH')
 
 })
 const PORT = process.env.PORT || 8080
